3f88533e2c656926e8be51db5f4a44eeff4e3a31
dhanggas <dhanggas@gmail.com>
2018-05-20 07:04:42 +0700

    konsep CRUD selesai

--- KonsepCRUD/Form1.vb
+++ KonsepCRUD/Form1.vb
@@ -37,7 +37,7 @@ Public Class Form1
         Try
             myCon = New OleDbConnection(strCon)
             myCon.Open()
-            strSQL = "insert into tbl_mahasiswa (KD_MHS,NM_MHS,ALAMAT_MHS)values('" & txtNim.Text & "','" & txtNama.Text & "','" & txtAlamat.Text & "')"
+            strSQL = "insert into tbl_mahasiswa (KD_MHS,NM_MHS,ALAMAT_MHS,JUR_MHS,TELP_MHS)values('" & txtNim.Text & "','" & txtNama.Text & "','" & txtAlamat.Text & "','" & txtJurusan.Text & "','" & txtTelpon.Text & "')"
             objCommand = New OleDbCommand(strSQL, myCon)
             If objCommand.ExecuteNonQuery() Then
                 MsgBox("Data Tersimpan")
@@ -75,4 +75,73 @@ Public Class Form1
         txtTelpon.Text = ""
     End Sub
 
+    Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
+
+    End Sub
+
+    Private Sub grdMahasiswa_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles grdMahasiswa.CellContentClick
+
+    End Sub
+
+    Private Sub grdMahasiswa_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles grdMahasiswa.Click
+        Try
+
+            txtNim.Text = IIf(IsDBNull(grdMahasiswa.SelectedCells(0).Value), " ", grdMahasiswa.SelectedCells(0).Value)
+            txtNama.Text = IIf(IsDBNull(grdMahasiswa.SelectedCells(1).Value), " ", grdMahasiswa.SelectedCells(1).Value)
+            txtJurusan.Text = IIf(IsDBNull(grdMahasiswa.SelectedCells(2).Value), " ", grdMahasiswa.SelectedCells(2).Value)
+            txtAlamat.Text = IIf(IsDBNull(grdMahasiswa.SelectedCells(3).Value), " ", grdMahasiswa.SelectedCells(3).Value)
+            txtTelpon.Text = IIf(IsDBNull(grdMahasiswa.SelectedCells(4).Value), " ", grdMahasiswa.SelectedCells(4).Value)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub btnEdit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEdit.Click
+        Try
+
+            If btnEdit.Text = "EDIT" Then
+                btnEdit.Text = "UPDATE"
+            Else
+                myCon = New OleDbConnection(strCon)
+                myCon.Open()
+                strSQL = "update tbl_mahasiswa set NM_MHS ='" & txtNama.Text & "',ALAMAT_MHS='" & txtAlamat.Text & "',JUR_MHS='" & txtJurusan.Text & "',TELP_MHS='" & txtJurusan.Text & "' WHERE KD_MHS= '" & txtNim.Text & "'"
+                objCommand = New OleDbCommand(strSQL, myCon)
+                If objCommand.ExecuteNonQuery() Then
+                    MsgBox("Data TerUpdate")
+                Else
+                    MsgBox("Gagal DiUpdate")
+                End If
+
+
+                myCon.Close()
+                Call tampilkanData()
+                Call TextKosong()
+                btnEdit.Text = "EDIT"
+            End If
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub btnDelete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDelete.Click
+        Try
+            myCon = New OleDbConnection(strCon)
+            myCon.Open()
+            strSQL = "delete from tbl_mahasiswa WHERE KD_MHS= '" & txtNim.Text & "'"
+            objCommand = New OleDbCommand(strSQL, myCon)
+            If objCommand.ExecuteNonQuery() Then
+                MsgBox("Data TerHapus")
+            Else
+                MsgBox("Gagal DiHapus")
+            End If
+
+
+            myCon.Close()
+            Call tampilkanData()
+            Call TextKosong()
+        Catch ex As Exception
+
+        End Try
+    End Sub
 End Class
